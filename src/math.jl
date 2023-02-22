1d5e4af33fb99b1701b67f6ee86012b2364b286a
Tarn Yeong Ching <i@ctarn.io>
2023-02-22 17:57:47 +0800

    refactor

--- src/math.jl
+++ src/math.jl
@@ -5,6 +5,29 @@ error_ppb(a, b) = error_rel(a, b) * 1e9
 # moe: margin of error
 in_moe(a, b, ε) = abs(a - b) <= max(abs(a), abs(b)) * ε
 
+m_to_mz(m, z) = (m + mₚ * z) / abs(z)
+mz_to_m(mz, z) = mz * abs(z) - mₚ * z
+
+mh_to_mz(mh, z) = begin
+    if z > 0
+        (mh + mₚ * (z - 1)) / z
+    elseif z < 0
+        (mh - mₚ * (-z - 1)) / -z
+    else
+        0.0
+    end
+end
+
+mz_to_mh(mz, z) = begin
+    if z > 0
+        mz * z - mₚ * (z - 1)
+    elseif z < 0
+        mz * -z + mₚ * (-z - 1)
+    else
+        0.0
+    end
+end
+
 calc_centroid(xs, ws) = begin
     if length(xs) > 1
         ss = (ws[begin:end-1] .+ ws[begin+1:end]) .* (xs[begin+1:end] .- xs[begin:end-1])
